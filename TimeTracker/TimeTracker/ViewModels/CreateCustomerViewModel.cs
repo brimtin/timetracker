2be77bbca9853b2845571e8a2ba8c1acdbd1052f
Shod <dilshod.sadiev@outlook.com>
2019-09-30 20:38:24 -0400

    Customers - Edit and Delete

---
+++ TimeTracker/TimeTracker/ViewModels/CreateCustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTracker.ViewModels
+{
+    public class CreateCustomerViewModel
+    {
+        public int ClientID { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyPhone { get; set; }
+        public string CompanyPOC { get; set; }
+    }
+}
