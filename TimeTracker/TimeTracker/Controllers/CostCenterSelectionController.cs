22ba365da95cf09ce3dffc049bcf22f6e62db581
Thomas C. Lowry <thomas.c.lowry@outlook.com>
2019-10-01 10:58:52 -0400

    End of Shod's changes

---
+++ TimeTracker/TimeTracker/Controllers/CostCenterSelectionController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Data;
+
+namespace TimeTracker.Controllers
+{
+    public class CostCenterSelectionController : Controller
+    {
+        private Tracker tracker;
+
+        public CostCenterSelectionController(Tracker tracker)
+        {
+            this.tracker = tracker;
+        }
+
+        public IActionResult Index()
+        {
+            var cost = tracker.Projects.Where(x => x.ClientID == id).FirstOrDefault();
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            Client model = new Client { ClientID = customer.ClientID, CompanyName = customer.CompanyName, CompanyPhone = customer.CompanyPhone, CompanyPoc = customer.CompanyPoc };
+            return View();
+        }
+
+        public IActionResult Start()
+        {
+            try
+            {
+                Session session = new Session { StartTime = DateTime.Now, EndTime = DateTime.MaxValue, EmployeeID = 1, ProjectID = 1 };
+                tracker.Sessions.Add(session);
+                tracker.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
